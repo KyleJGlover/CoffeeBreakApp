fb589381d7544addc4b91785ca2cf2ca1794e060
Kyle J Glover <kjglover4585@gmail.com>
2021-06-25 18:56:46 -0700

    Initial Commit

---
+++ CoffeeBreakApp/CoffeeBreakAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeBreakAppApp.swift
+//  CoffeeBreakApp
+//
+//  Created by Kyle Glover on 6/25/21.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeBreakAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
